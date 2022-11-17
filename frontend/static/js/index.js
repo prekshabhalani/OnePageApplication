9e9bbeb9de48df33cb98a85bf97a3a29119ec274
Preksha Bhalani <preksha.b>
2022-11-17 23:32:43 +0530

    Easy Debuging : basic routing consoling and setting for every routing

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,34 @@
 /*****To handle all the js entry point */
+const router = async () => {
+    const routers = [
+        { path: "/", view: () => console.log("Dashboard view") },
+        { path: "/posts", view: () => console.log("You mean post") },
+        { path: "/settings", view: () => console.log("You mean settings") }
 
-console.log("On front end side");
+    ]
+
+    const potentialMatch = routers.map(route => {
+        return {
+            route: route.path,
+            content: route.view,
+            isMatch: location.pathname === route.path
+        }
+    })
+    let matchRoute = potentialMatch.find(match => match.isMatch)
+
+    //Something like 404 if not match default routing set
+    if (!matchRoute) {
+        matchRoute = {
+            route: routers[0].path,
+            content: routers[0].view,
+            isMatch: true
+        }
+    }
+
+    console.table(matchRoute)//Match routing
+    console.table(potentialMatch) //ALL routing
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    router()
+})
